8d0a39a3bb2acddcb4ffe5a0e79e7f01f8e3b183
d42f <dmitri.frolof@gmail.com>
2024-01-02 00:10:15 +0100

    refactored scripts

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { useAnchorObserver } from './useAnchorObserver';
+export { useVisible, useVisibleChildren } from './useIntersectionObserver';
+export { useWasScrolled } from './useWasScrolled';
